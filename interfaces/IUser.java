42c43eb090f8f6138aee08454eb6731205662847
alexsilvar <alexskysme@gmail.com>
2017-09-30 18:20:32 -0300

    Interfaces usadas

---
+++ interfaces/IUser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_1.interfaces;
+
+/**
+ *
+ * @author Avell B155 MAX
+ */
+public interface IUser {
+
+    public void getMessage(String msg);
+
+}
